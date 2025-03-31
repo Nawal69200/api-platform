dbe5dead951e7c3071c900eab0a09aedbe8beac4
Nawal69200 <nawal.kajtou@gmail.com>
2025-03-31 20:53:49 +0200

    initial commit

--- cook-api/config/bundles.php
+++ cook-api/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
